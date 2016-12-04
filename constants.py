14838992dcef2ca0168b79ef7b47a8b5abb17ead
Pranav Vaidik Dhulipala <pranav.vaidik@gmail.com>
2016-12-04 15:36:33 -0600

    putting all the constants into a single file for ease of access

---
+++ constants.py
@@ -0,0 +1,12 @@
+#Defining Constants reading images
+PATH = "CUB_200_2011/CUB_200_2011/images/"
+
+
+#Definfing constants for segmentation
+N_SEGMENTS = 250
+
+#Defining constants for Training the datasets
+
+
+
+# Defining constants for Testing
