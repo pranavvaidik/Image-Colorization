a111532b1ac4fb2d8abdf72982e28ce142c99d5f
Pranav Vaidik Dhulipala <pranav.vaidik@gmail.com>
2016-12-04 15:44:32 -0600

    added extract_features and get_subsquares methods to the imutils file. still need to add code part to the methods

--- imutils.py
+++ imutils.py
@@ -3,13 +3,13 @@ import numpy as np
 from skimage.segmentation import slic, mark_boundaries
 from constants import *
 
-# this method takes the path to the folder as input and loads all the files in the folder into the dataset
+# this method takes the PATH to the folder as input and loads all the files in the folder into the dataset
 def load_data():
 	
 	image_locs = np.genfromtxt("images.txt",dtype = None)
-	#imagePath = path + image_locs[1][1]
+	#imagePATH = PATH + image_locs[1][1]
 	for i in range(len(image_locs)):
-		imagePath = path + image_locs[i][1]
+		imagePath = PATH + image_locs[i][1]
 		#print imagePath		
 		image = cv2.imread(imagePath)
 		#print image
@@ -22,4 +22,8 @@ def segment_image(image):
 	segments = slic(img, n_segments=N_SEGMENTS, compactness=10, sigma=1)
 	return segments
 
-	
+def get_subsquares(image):
+	return
+
+def extract_features(paths):
+	return
