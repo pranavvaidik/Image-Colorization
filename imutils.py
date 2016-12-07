1f007bbf0c27c8007cf03c2cc4c7f36c2ace2ab6
Pranav Vaidik Dhulipala <pranav.vaidik@gmail.com>
2016-12-06 19:03:28 -0600

    changed everything to YUV format

--- imutils.py
+++ imutils.py
@@ -8,77 +8,86 @@ def load_training_data():
 	image_locs = np.genfromtxt("images.txt",dtype = None)
 	#imagePATH = PATH + image_locs[1][1]
 	features = np.array([]).reshape(0, SQUARE_SIZE * SQUARE_SIZE)
-        train_L = np.array([])
-        train_a = np.array([])
-        train_b = np.array([])
+        #train_L = np.array([])
+        #train_a = np.array([])
+        #train_b = np.array([])
+	train_Y = np.array([])
+	train_U = np.array([])
+	train_V = np.array([])	
+
 	for i in range(len(image_locs)):
 		imagePath = PATH + image_locs[i][1]
 		print "loading data from "+ imagePath + " ... "
-		subsquares, L, a, b = extract_features(imagePath)
+		#subsquares, L, a, b = extract_features(imagePath)
+		subsquares, Y,U,V = extract_features(imagePath)
 		features = np.concatenate((features, subsquares), axis=0)
-            	train_L = np.concatenate((train_L, L), axis=0)
-            	train_a = np.concatenate((train_a, a), axis=0)
-		train_b = np.concatenate((train_b, b), axis=0)
+            	train_Y = np.concatenate((train_Y, Y), axis=0)
+            	train_U = np.concatenate((train_U, U), axis=0)
+		train_V = np.concatenate((train_V, V), axis=0)
+
+		
+		
 		#image = cv2.imread(imagePath)
 		#print image
 		#cv2.imshow("image",image)
 		#cv2.waitKey(100)
-	return features, train_L, train_a, train_b #will be returning the training and testing sets after this
+	#return features, train_L, train_a, train_b #will be returning the training and testing sets after this
+	return features, train_Y, train_U, train_V #will be returning the training and testing sets after this
 
 def load_test_data():
 	image_locs = np.genfromtxt("images.txt",dtype = None)
 	#imagePATH = PATH + image_locs[1][1]
 	features = np.array([]).reshape(0, SQUARE_SIZE * SQUARE_SIZE)
-        test_L = np.array([])
-        test_a = np.array([])
-        test_b = np.array([])
+        test_Y = np.array([])
+        test_U = np.array([])
+        test_V = np.array([])
 	for i in range(len(image_locs)):
 		imagePath = PATH + image_locs[i][1]
 		#print imagePath
-		subsquares, L, a, b = extract_features(imagePath)
+		subsquares, Y,U,V = extract_features(imagePath)
 		features = np.concatenate((features, subsquares), axis=0)
-            	test_L = np.concatenate((test_L, L), axis=0)
-            	test_a = np.concatenate((test_a, a), axis=0)
-		test_b = np.concatenate((test_b, b), axis=0)
+            	test_Y = np.concatenate((test_Y, Y), axis=0)
+            	test_U = np.concatenate((test_U, U), axis=0)
+		test_V = np.concatenate((test_V, V), axis=0)
 		#image = cv2.imread(imagePath)
 		#print image
 		#cv2.imshow("image",image)
 		#cv2.waitKey(100)
-	return features, test_L, test_a, test_b #will be returning the training and testing sets after this
+	return features, test_Y, test_U, test_V #will be returning the training and testing sets after this
 	
 
 def segment_image(path):
 	image = cv2.imread(path)	
 	gray_image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-	Lab  = cv2.cvtColor(image,cv2.COLOR_BGR2LAB)
+	yuv  = cv2.cvtColor(image,cv2.COLOR_BGR2YUV)
 	segments = slic(gray_image, n_segments=N_SEGMENTS, compactness=0.1, sigma=1)
-	return gray_image, Lab, segments
+	return gray_image, yuv, segments
 
 def extract_features(path):
 	#image = cv2.imread(path)
 	#segments = slic(gray_image, n_segments=N_SEGMENTS, compactness=10, sigma=1)
-	gray_image, Lab , segments = segment_image(path)
+	gray_image, yuv , segments = segment_image(path)
 	n_segments = segments.max() + 1
 
 	#compute centroids
 	point_count = np.zeros(n_segments)
 	centroids = np.zeros((n_segments, 2))
-    	L = np.zeros(n_segments)
-    	a = np.zeros(n_segments)
-    	b = np.zeros(n_segments)
+    	Y = np.zeros(n_segments)
+    	U = np.zeros(n_segments)
+    	V = np.zeros(n_segments)
 	for (i,j), value in np.ndenumerate(segments):
 		point_count[value] += 1
 		centroids[value][0] += i
 		centroids[value][1] += j
-		L[value] += Lab[i][j][0]        
-		a[value] += Lab[i][j][1]
-		b[value] += Lab[i][j][2]
+		Y[value] += yuv[i][j][0]        
+		U[value] += yuv[i][j][1]
+		V[value] += yuv[i][j][2]
 
 	for k in range(n_segments):
 		centroids[k] /= point_count[k]
-		L[k] /= point_count[k]
-		a[k] /= point_count[k]
-		b[k] /= point_count[k]
+		Y[k] /= point_count[k]
+		U[k] /= point_count[k]
+		V[k] /= point_count[k]
 
 	subsquares = np.zeros((n_segments, SQUARE_SIZE * SQUARE_SIZE))
 	for k in range(n_segments):
@@ -94,4 +103,44 @@ def extract_features(path):
                 		subsquares[k][i*SQUARE_SIZE + j] = gray_image[i + top][j + left]
         	subsquares[k] = np.fft.fft2(subsquares[k].reshape(SQUARE_SIZE, SQUARE_SIZE)).reshape(SQUARE_SIZE * SQUARE_SIZE)
 
-	return np.abs(subsquares), L, a, b
+	return np.abs(subsquares), Y,U,V
+
+def predict_image(svr_Y, svr_U, svr_V, path):
+	image = cv2.imread(path)	
+	gray_image, yuv , segments = segment_image(path)
+	n_segments = segments.max() + 1
+	subsquares, Y,U,V = extract_features(path)
+	
+	Y_image = np.zeros_like(gray_image)
+	U_image = np.zeros_like(gray_image)
+	V_image = np.zeros_like(gray_image)
+	#prediction of LAB
+	predicted_Y = np.zeros(n_segments)
+    	predicted_U = np.zeros(n_segments)
+    	predicted_V = np.zeros(n_segments)	
+	for k in range(n_segments):
+		predicted_Y[k] = svr_Y.predict(subsquares[k])
+		predicted_U[k] = svr_U.predict(subsquares[k])
+		predicted_V[k] = svr_V.predict(subsquares[k])
+
+	# Apply MRF to smooth out colorings
+#    	predicted_u, predicted_v = apply_mrf(predicted_u, predicted_v, segments, n_segments, img, subsquares)
+	
+	print "problem is here"
+
+	#image reconstruction	
+	for (i,j), value in np.ndenumerate(segments):
+		Y_image[i,j] = predicted_Y[value]
+		
+		U_image[i,j] = predicted_U[value]
+		V_image[i,j] = predicted_V[value]
+    	#rgb = retrieveRGB(yuv)
+	Y_image = gray_image	
+	merged = cv2.merge([Y_image,U_image,V_image])
+	
+	predicted_image = cv2.cvtColor(merged,cv2.COLOR_YUV2BGR)
+	print "predicted"
+	cv2.imshow("Y",Y_image)
+
+	return predicted_image
+
