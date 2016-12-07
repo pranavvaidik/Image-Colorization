81edb1b7b262ef474c2994182ed811eea838050b
Pranav Vaidik Dhulipala <pranav.vaidik@gmail.com>
2016-12-06 19:03:59 -0600

    changed dataset

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
 import cv2
-import imutils
+from imutils import *
+from constants import *
+from sklearn.svm import SVR
+
+
+path = "TrainingData/1.jpg"
+#cv2.figure(2)
+img = cv2.imread(path)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("gray",gray)
+cv2.waitKey(0)
+features, train_L, train_a, train_b = load_training_data()
+
+
+L_svr = SVR(C=C, epsilon=SVR_EPSILON)
+a_svr = SVR(C=C, epsilon=SVR_EPSILON)
+b_svr = SVR(C=C, epsilon=SVR_EPSILON)
+
+
+L_svr.fit(features,train_L)
+print "L trained"
+a_svr.fit(features,train_L)
+print "a trained"
+b_svr.fit(features,train_L)
+print "b trained"
+
+
+
+
+
+predicted_image = predict_image(L_svr, a_svr, b_svr, path)
+
+cv2.imshow("predicted colors", predicted_image*255)
+cv2.waitKey(0)
+
+
 
